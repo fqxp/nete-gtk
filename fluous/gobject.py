e4989908316e9552d44756446da2b3c296992231
Frank Ploss <frank@fqxp.de>
2016-04-04 18:17:18 +0200

    Change 'connect' default parameter to a correct value

--- fluous/gobject.py
+++ fluous/gobject.py
@@ -9,7 +9,7 @@ def set_props(component, props):
     component.thaw_notify()
 
 
-def connect(Component, map_state_to_props, map_dispatch_to_props={}):
+def connect(Component, map_state_to_props, map_dispatch_to_props=lambda dispatch: {}):
 
     def create_component(store, *args, **kwargs):
         if 'build_component' in getargspec(Component).args:
@@ -21,6 +21,11 @@ def connect(Component, map_state_to_props, map_dispatch_to_props={}):
 
         # connect GObject signals to handlers
         for signal, signal_handler in map_dispatch_to_props(store.dispatch).items():
+            # GI isn’t able to handle variable argument lists, so we’re passing
+            # an optional list of a maximum number of arguments. The signal
+            # handler is provided as a default argument - this is a way to pass
+            # a changing reference from the current context to the function
+            # we’re defining.
             def handler(source, arg0=None, arg1=None, arg2=None, arg3=None, signal_handler=signal_handler):
                 args_count = signal_handler.__code__.co_argcount
                 handler_args = [arg0, arg1, arg2, arg3][:args_count]
