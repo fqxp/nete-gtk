906a55671bb522a92709beaac8cb7b3102302d52
Frank Ploss <mail@fqxp.de>
2019-07-02 11:28:32 +0200

    Add typing hints to selectors

--- nete/gtkgui/state/selectors.py
+++ nete/gtkgui/state/selectors.py
@@ -1,19 +1,27 @@
 from functools import lru_cache
+from typing import List, Union
+from .models import (
+    Note,
+    NoteCollection,
+    NoteListItem,
+    State,
+    Ui,
+)
 
 
 @lru_cache()
-def current_note_collection(state):
+def current_note_collection(state: State) -> NoteCollection:
     current_note_collection_id = state['ui']['current_note_collection_id']
     return state['note_collections'][current_note_collection_id]
 
 
 @lru_cache()
-def current_note(state):
+def current_note(state: State) -> Union[Note, None]:
     return state.get('current_note')
 
 
 @lru_cache()
-def visible_notes(state):
+def visible_notes(state: State) -> List[NoteListItem]:
     return [
         note_list_item
         for note_list_item in state['note_list']['notes']
@@ -22,25 +30,25 @@ def visible_notes(state):
 
 
 @lru_cache()
-def note_list_first(state):
+def note_list_first(state: State) -> Union[Note, None]:
     notes = visible_notes(state)
     return notes[0] if len(notes) > 0 else None
 
 
 @lru_cache()
-def note_list_last(state):
+def note_list_last(state: State) -> Union[Note, None]:
     notes = visible_notes(state)
     return notes[-1] if len(notes) > 0 else None
 
 
 @lru_cache()
-def note_list_contains(state, note_title):
+def note_list_contains(state: State, note_title: str) -> bool:
     notes = visible_notes(state)
     return any(note['title'] == note_title for note in notes)
 
 
 @lru_cache()
-def note_list_next(state, note_title):
+def note_list_next(state: State, note_title: str) -> Union[Note, None]:
     notes = visible_notes(state)
     for i, note in enumerate(notes):
         if note['title'] == note_title:
@@ -49,7 +57,10 @@ def note_list_next(state, note_title):
 
 
 @lru_cache()
-def note_list_previous(state, current_note_title):
+def note_list_previous(
+        state: State,
+        current_note_title: str
+        ) -> Union[Note, None]:
     notes = visible_notes(state)
     for i, note in enumerate(notes):
         if note['title'] == current_note_title:
@@ -58,5 +69,5 @@ def note_list_previous(state, current_note_title):
 
 
 @lru_cache()
-def ui_state(state):
+def ui_state(state: State) -> Ui:
     return state['ui']
