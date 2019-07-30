e74cff623c1bc57bee0d949998ed17456b9d2c43
Frank Ploss <mail@fqxp.de>
2019-07-30 12:24:16 +0200

    Sort reducer by action name

--- nete/gui/reducer.py
+++ nete/gui/reducer.py
@@ -12,17 +12,32 @@ from nete.gui.state.models import NoteListItem
 
 
 reduce = create_reducer({
-    ActionType.SELECT_NOTE: lambda state, action:
-        state.transform(
-            ['current_note'], action['note'],
-            ['ui', 'focus'], 'note_view',
-            ['note_list', 'preselected_note_title'], None,
-        ),
+    ActionType.CANCEL_EDIT_NOTE_TITLE: lambda state, action: (
+        state.transform(['ui', 'focus'], 'note_view')),
 
-    ActionType.PRESELECT_NOTE: lambda state, action:
+    ActionType.CHANGE_CURSOR_POSITION: lambda state, action:
         state.transform(
-            ['note_list', 'preselected_note_title'], action['note_title']
-        ),
+            ['current_note', 'cursor_position'], action['cursor_position']
+        ) if state['current_note'] else state,
+
+    ActionType.CHANGE_FILTER_TERM: lambda state, action:
+        state.transform(
+            ['note_list', 'notes', ny], (
+                lambda note: note.set('visible',
+                                      is_visible(note['title'],
+                                                 action['filter_term']))),
+            ['note_list', 'filter_term'], action['filter_term'],
+            ['note_list', 'preselected_note_title'], (
+                preselected_note(state['note_list'], action['filter_term']))),
+
+    ActionType.CHANGE_NOTE_TEXT: lambda state, action:
+        state.transform(
+            ['current_note', 'text'], action['text'],
+            ['current_note', 'needs_save'], True
+        ) if state['current_note'] else state,
+
+    ActionType.CLOSE_NOTE: lambda state, action:
+        state.transform(['current_note'], None),
 
     ActionType.CREATED_NOTE: lambda state, action:
         state.transform(
@@ -40,11 +55,8 @@ reduce = create_reducer({
                 without(state['note_list']['notes'], action['note_title']))
         ),
 
-    ActionType.CHANGE_NOTE_TEXT: lambda state, action:
-        state.transform(
-            ['current_note', 'text'], action['text'],
-            ['current_note', 'needs_save'], True
-        ) if state['current_note'] else state,
+    ActionType.FINISH_EDIT_NOTE_TEXT: lambda state, action: (
+        state.transform(['ui', 'focus'], 'note_view')),
 
     ActionType.FINISH_EDIT_NOTE_TITLE: lambda state, action:
         state.transform(
@@ -67,32 +79,6 @@ reduce = create_reducer({
             ),
         ),
 
-    ActionType.CANCEL_EDIT_NOTE_TITLE: lambda state, action: (
-        state.transform(['ui', 'focus'], 'note_view')),
-
-    ActionType.CHANGE_CURSOR_POSITION: lambda state, action:
-        state.transform(
-            ['current_note', 'cursor_position'], action['cursor_position']
-        ) if state['current_note'] else state,
-
-    ActionType.NOTE_SAVED: lambda state, action:
-        state.transform(
-            ['current_note', 'needs_save'], False
-        ) if state['current_note'] else state,
-
-    ActionType.CLOSE_NOTE: lambda state, action:
-        state.transform(['current_note'], None),
-
-    ActionType.SELECT_NOTE_COLLECTION: lambda state, action:
-        state.transform(
-            ['current_note'], None,
-            ['ui', 'current_note_collection_id'], action['collection_id'],
-            ['ui', 'focus'], 'filter_term_entry',
-            ['note_list', 'preselected_note_title'], None,
-            ['note_list', 'notes'], ordered(action['notes']),
-            ['ui', 'focus'], 'filter_term_entry',
-        ),
-
     ActionType.FOCUS: lambda state, action:
         state.transform(
             ['ui', 'focus'], action['widget_name'],
@@ -107,34 +93,27 @@ reduce = create_reducer({
     #                 state['note_list']['filter_term']))
     #     ),
 
-    ActionType.TOGGLE_EDIT_NOTE_TEXT: lambda state, action:
-        state.transform(
-            ['ui', 'focus'], (
-                'note_editor' if state['ui']['focus'] != 'note_editor'
-                else 'note_view')
-        ) if state['current_note'] is not None else state,
-
-    ActionType.FINISH_EDIT_NOTE_TEXT: lambda state, action: (
-        state.transform(['ui', 'focus'], 'note_view')),
-
-    ActionType.TOGGLE_EDIT_NOTE_TITLE: lambda state, action:
-        state.transform(
-            ['ui', 'focus'], (
-                'note_title_editor' if state['ui']['focus'] != 'note_title_editor'
-                else 'note_view')
-        ) if state['current_note'] is not None else state,
-
-    ActionType.MOVE_PANED_POSITION: lambda state, action: (
-        state.transform(['ui', 'paned_position'], action['position'])),
-
-    ActionType.LOADED_UI_STATE: lambda state, action:
-        state.transform(['ui'], lambda c: c.update(action['ui'])),
-
     ActionType.LOAD_CONFIGURATION: lambda state, action:
         state.transform(
             ['configuration'], action['configuration'],
             ['ui', 'current_note_collection_id'], (
                 list(action['configuration'].note_collections)[0].id)
+        ),
+
+    ActionType.LOADED_UI_STATE: lambda state, action:
+        state.transform(['ui'], lambda c: c.update(action['ui'])),
+
+    ActionType.MOVE_PANED_POSITION: lambda state, action: (
+        state.transform(['ui', 'paned_position'], action['position'])),
+
+    ActionType.NOTE_SAVED: lambda state, action:
+        state.transform(
+            ['current_note', 'needs_save'], False
+        ) if state['current_note'] else state,
+
+    ActionType.PRESELECT_NOTE: lambda state, action:
+        state.transform(
+            ['note_list', 'preselected_note_title'], action['note_title']
         ),
 
     ActionType.RESET: lambda state, action:
@@ -143,15 +122,36 @@ reduce = create_reducer({
             ['note_list', 'preselected_note_title'], None,
         ),
 
-    ActionType.CHANGE_FILTER_TERM: lambda state, action:
+    ActionType.SELECT_NOTE: lambda state, action:
         state.transform(
-            ['note_list', 'notes', ny], (
-                lambda note: note.set('visible',
-                                      is_visible(note['title'],
-                                                 action['filter_term']))),
-            ['note_list', 'filter_term'], action['filter_term'],
-            ['note_list', 'preselected_note_title'], (
-                preselected_note(state['note_list'], action['filter_term']))),
+            ['current_note'], action['note'],
+            ['ui', 'focus'], 'note_view',
+            ['note_list', 'preselected_note_title'], None,
+        ),
+
+    ActionType.SELECT_NOTE_COLLECTION: lambda state, action:
+        state.transform(
+            ['current_note'], None,
+            ['ui', 'current_note_collection_id'], action['collection_id'],
+            ['ui', 'focus'], 'filter_term_entry',
+            ['note_list', 'preselected_note_title'], None,
+            ['note_list', 'notes'], ordered(action['notes']),
+            ['ui', 'focus'], 'filter_term_entry',
+        ),
+
+    ActionType.TOGGLE_EDIT_NOTE_TEXT: lambda state, action:
+        state.transform(
+            ['ui', 'focus'], (
+                'note_editor' if state['ui']['focus'] != 'note_editor'
+                else 'note_view')
+        ) if state['current_note'] is not None else state,
+
+    ActionType.TOGGLE_EDIT_NOTE_TITLE: lambda state, action:
+        state.transform(
+            ['ui', 'focus'], (
+                'note_title_editor' if state['ui']['focus'] != 'note_title_editor'
+                else 'note_view')
+        ) if state['current_note'] is not None else state,
 })
 
 
